e8bdead816c6d63aeb08e2552d271dbd2d0dd940
edwin <edwrodrig@gmail.com>
2020-06-08 19:17:59 -0400

    Coverage fix

--- test/ElementBase.test.js
+++ test/ElementBase.test.js
@@ -10,6 +10,10 @@ test("handle plain element", () => {
     let element = sero.get('test');
 
     expect(element.value).toEqual(null);
+
+    element.value = null;
+    expect(element.value).toEqual(null);
+
     element.setValueProperty({
        get() { return outVar; },
        set(value) { outVar = value; }
